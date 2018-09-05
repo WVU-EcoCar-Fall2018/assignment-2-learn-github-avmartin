40778b4630cafbc05afd43c509dc42de32250878
Alex Martin <alex@oem-VirtualBox>
2018-09-05 16:12:41 -0400

    finished assignment

---
+++ include/hellomake.h
@@ -0,0 +1,13 @@
+/*
+example include file
+*/
+
+// Copyright [2018] <Alex Martin>
+
+
+#ifndef INCLUDE_HELLOMAKE_H_
+#define INCLUDE_HELLOMAKE_H_
+
+void myPrintHelloMake(void);
+
+#endif  // INCLUDE_HELLOMAKE_H_
